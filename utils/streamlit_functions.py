28af431e333ad389f743a0fa336fea43c597b8dd
GatmezeiKornel <gatmezei.kornel@gmail.com>
2024-03-13 14:44:14 +0100

    test with secrets.toml

--- utils/streamlit_functions.py
+++ utils/streamlit_functions.py
@@ -54,7 +54,7 @@ def document_displayer(blob_storage, files, session_state_data):
 
 def password_entered():
     """Checks whether a password entered by the user is correct."""
-    if hmac.compare_digest(st.session_state["password"], os.environ['streamlit_password']):
+    if hmac.compare_digest(st.session_state["password"], st.secrets['password']):
         st.session_state["password_correct"] = True
         del st.session_state["password"]  # Don't store the password.
     else:
