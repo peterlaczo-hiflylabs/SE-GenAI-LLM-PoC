57ac5cf6f0b0a5cd3babebc5d847041991dedc1e
GatmezeiKornel <gatmezei.kornel@gmail.com>
2024-03-21 08:17:00 +0100

    hotfix

--- utils/streamlit_functions.py
+++ utils/streamlit_functions.py
@@ -50,8 +50,8 @@ def format_button_style():
         unsafe_allow_html=True,
     )
 
-def document_displayer(blob_storage, selected_container, session_state, doc_type):
-    match doc_type:
+def document_displayer(blob_storage, selected_container, session_state, document_type):
+    match document_type:
         case "anam":
             session_state_data = session_state.anam_html_table_name
             row_num = session_state.anam_row_index
@@ -91,58 +91,58 @@ def check_password():
 
     # Show input for password.
     st.text_input(
-        "Password", doc_type="password", on_change=password_entered, key="password"
+        "Password", type="password", on_change=password_entered, key="password"
     )
     if "password_correct" in st.session_state:
         st.error("Password incorrect")
     return False
 
-def csv_upload_from_dataframe(blob_storage, storage: pd.DataFrame, session_state, timestamp: str, doc_type):
+def csv_upload_from_dataframe(blob_storage, storage: pd.DataFrame, session_state, timestamp: str, document_type):
     csv_string_buffer = io.StringIO()
     storage.to_csv(csv_string_buffer, index=False, sep=';')
     csv_string = csv_string_buffer.getvalue()
-    success, error = upload_to_blob_storage(blob_storage, session_state.selected_container,f"{session_state.selected_id}/cache/{session_state.selected_id}_{timestamp}_{doc_type}_feedbacks.csv",csv_string)
-    if success and "extended" not in doc_type:
+    success, error = upload_to_blob_storage(blob_storage, session_state.selected_container,f"{session_state.selected_id}/cache/{session_state.selected_id}_{timestamp}_{document_type}_feedbacks.csv",csv_string)
+    if success and "extended" not in document_type:
         st.write("FILE UPLOADED")
     # else:
     #     st.write(error)
 
-def search_for_log(blob_storage, session_state, doc_type, timestamp):
-    feedback_storage_source = [file for file in session_state.files if file['name'].split('/')[1] == 'cache' and f"{doc_type}_feedbacks" in file['name'] and timestamp in file['name']]
+def search_for_log(blob_storage, session_state, document_type, timestamp):
+    feedback_storage_source = [file for file in session_state.files if file['name'].split('/')[1] == 'cache' and f"{document_type}_feedbacks" in file['name'] and timestamp in file['name']]
     if len(feedback_storage_source) > 0:
             feedback_storage = pd.read_csv(io.StringIO(select_blob_file(blob_storage, session_state.selected_container,feedback_storage_source[0])), sep=';')
     else:
         feedback_storage = pd.DataFrame()
     return feedback_storage
 
-def block_feedback(blob_storage, gen_csv, session_state, doc_type):
-    match doc_type:
+def block_feedback(blob_storage, gen_csv, session_state, document_type):
+    match document_type:
         case "anam":
             row_num = session_state.anam_row_index
         case "gyogyszer":
             row_num = session_state.gyogyszer_row_index
 
     refreshed = False
-    if st.checkbox("Visszajelzés adása", key=f"{doc_type}_curr", value=False):
+    if st.checkbox("Visszajelzés adása", key=f"{document_type}_curr", value=False):
         session_state.files = [file for file in list_files_in_container(blob_storage, session_state.selected_container) if len(file['name'].split('/')) > 2 and session_state.selected_id in file['name'].split('/')[-1]]
         timestamp = [file for file in st.session_state.files if file['name'].split('/')[1] == 'cache' and 'anamnezis_of' in file['name']][-1]['name'].split('/')[-1].split('_')[-1].split('.')[0]
         refreshed = True
 
-        feedback_storage = search_for_log(blob_storage,session_state, doc_type, timestamp)
-        extended_feedback_storage = search_for_log(blob_storage,session_state,f"{doc_type}_extended",timestamp)
-        feedback_name = st.text_input("Kérlek írd be a neved:", value="", key=f"{doc_type}_name")
+        feedback_storage = search_for_log(blob_storage,session_state, document_type, timestamp)
+        extended_feedback_storage = search_for_log(blob_storage,session_state,f"{document_type}_extended",timestamp)
+        feedback_name = st.text_input("Kérlek írd be a neved:", value="", key=f"{document_type}_name")
         cols =  st.columns((1.1,1.5,1.1,1,1))
         check_box_answers = [""] * 5
         if row_num != "":
-            check_box_answers[0] = cols[0].checkbox("Általános megjegyzés?", key=f"{doc_type}_alt")
+            check_box_answers[0] = cols[0].checkbox("Általános megjegyzés?", key=f"{document_type}_alt")
             if check_box_answers[0] == False:
-                check_box_answers[1] = cols[1].checkbox("Helytelenül szerepel a táblában?", key=f"{doc_type}_")
-                check_box_answers[2] = cols[2].checkbox("Duplikátum", key=f"{doc_type}_dupl")
-                check_box_answers[3] = cols[3].checkbox("Helytelen dátum?", key=f"{doc_type}_date")
-                check_box_answers[4] = cols[4].checkbox("Rossz BNO kód?", key=f"{doc_type}_bno")
-        feedback_text = st.text_area("Kérlek írd be a visszajelzésed", value="",key=f"{doc_type}_desc")
+                check_box_answers[1] = cols[1].checkbox("Helytelenül szerepel a táblában?", key=f"{document_type}_")
+                check_box_answers[2] = cols[2].checkbox("Duplikátum", key=f"{document_type}_dupl")
+                check_box_answers[3] = cols[3].checkbox("Helytelen dátum?", key=f"{document_type}_date")
+                check_box_answers[4] = cols[4].checkbox("Rossz BNO kód?", key=f"{document_type}_bno")
+        feedback_text = st.text_area("Kérlek írd be a visszajelzésed", value="",key=f"{document_type}_desc")
 
-        if st.button("Beküldés", key=f"{doc_type}_submitbtn"):
+        if st.button("Beküldés", key=f"{document_type}_submitbtn"):
             current_row = pd.DataFrame([{
                 'név': feedback_name,
                 'dátum': datetime.datetime.now().strftime("%Y/%m/%d"),
@@ -155,17 +155,17 @@ def block_feedback(blob_storage, gen_csv, session_state, doc_type):
                 'leírás': feedback_text
             }], index=[row_num-1 if row_num !=  "" else 0])
             feedback_storage = pd.concat([feedback_storage,current_row],ignore_index=True)
-            csv_upload_from_dataframe(blob_storage,feedback_storage, session_state, timestamp, doc_type)
+            csv_upload_from_dataframe(blob_storage,feedback_storage, session_state, timestamp, document_type)
             extended_log = create_extended_log(gen_csv, current_row)
             extended_feedback_storage = pd.concat([extended_feedback_storage,extended_log],ignore_index=True)
-            csv_upload_from_dataframe(blob_storage, extended_feedback_storage, session_state, timestamp, f"{doc_type}_extended")
+            csv_upload_from_dataframe(blob_storage, extended_feedback_storage, session_state, timestamp, f"{document_type}_extended")
 
-    if st.checkbox("Korábbi visszajelzések megjelenítése",key=f"{doc_type}_prev", value=False):
+    if st.checkbox("Korábbi visszajelzések megjelenítése",key=f"{document_type}_prev", value=False):
         if not refreshed:
             session_state.files = [file for file in list_files_in_container(blob_storage, session_state.selected_container) if len(file['name'].split('/')) > 2 and session_state.selected_id in file['name'].split('/')[-1]]
             timestamp = [file for file in st.session_state.files if file['name'].split('/')[1] == 'cache' and 'anamnezis_of' in file['name']][-1]['name'].split('/')[-1].split('_')[-1].split('.')[0]
         
-        feedback_storage = search_for_log(blob_storage,session_state, doc_type, timestamp)
+        feedback_storage = search_for_log(blob_storage,session_state, document_type, timestamp)
         if len(feedback_storage) > 0:
             st.table(feedback_storage)
         else:
